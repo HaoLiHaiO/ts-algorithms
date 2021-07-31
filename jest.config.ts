eea991cd9247a9618d56714b6579d735069af6c3
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-07-31 10:43:38 +0000

    set up ts for tests

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+    roots: ['<rootDir>'],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest",
+        "\\.ts$": "<rootDir>/node_modules/babel-jest"
+
+      },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    collectCoverage: true,
+    clearMocks: true,
+    coverageDirectory: "coverage",
+  };
