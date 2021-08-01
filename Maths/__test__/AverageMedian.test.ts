234ad8025e4e5c80835e314592e2b6b5349cdb59
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-01 09:54:12 +0000

    improve AverageMedian tests

--- Maths/__test__/AverageMedian.test.ts
+++ Maths/__test__/AverageMedian.test.ts
@@ -1,21 +1,15 @@
 import { averageMedian } from '../AverageMedian'
 
-test('should return the median of an array of numbers:', () => {
-  const medianValue = averageMedian([1, 2, 6, 4, 5])
-  expect(medianValue).toBe(4)
-})
+describe('Average median', () => {
+  it('returns the median of a sorted array of numbers with five items', () => {
+    expect(averageMedian([1, 2, 6, 4, 5])).toStrictEqual(4)
+  })
 
-test('should return the median of an array of numbers:', () => {
-  const medianValue = averageMedian([8, 9, 1, 2, 5, 10, 11])
-  expect(medianValue).toBe(8)
-})
+  it('returns the median of an unsorted array of numbers with seven items', () => {
+    expect(averageMedian([8, 9, 1, 2, 5, 10, 11])).toStrictEqual(8)
+  })
 
-test('should return the median of an array of numbers:', () => {
-  const medianValue = averageMedian([15, 18, 3, 9, 13, 5])
-  expect(medianValue).toBe(11)
-})
-
-test('should return the median of an array of numbers:', () => {
-  const medianValue = averageMedian([1, 2, 3, 4, 6, 8])
-  expect(medianValue).toBe(3.5)
+  it('returns the median of an array which contains negative numbers', () => {
+    expect(averageMedian([8, -9, 1, -2, 5, -10, 11])).toStrictEqual(1)
+  })
 })
